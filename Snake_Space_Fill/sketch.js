317ba66e7854830b89e5b1903cdb94abb32b7076
Balakrishna Prabhu B N <64630414+Bluejee@users.noreply.github.com>
2021-09-29 22:37:35 +0530

    changed the code to get full screen effect.

--- Snake_Space_Fill/sketch.js
+++ Snake_Space_Fill/sketch.js
@@ -1,10 +1,16 @@
 let segments = 50;
+//Segments the smaller side into as many portions
+
+let gridsize;
+
 let x;
 let y;
 let elc;
 function setup() {
-  screensize = min(windowWidth,windowHeight);
-  createCanvas(screensize,screensize);
+  // screensize = min(windowWidth,windowHeight);
+  // createCanvas(screensize,screensize);
+  createCanvas(windowWidth,windowHeight);
+  gridsize = min(width,height)/segments;
   background(255);
   // showGrid();
   
@@ -19,10 +25,11 @@ function setup() {
 function draw() {
   // frameRate(4)
   // background(9, 132, 227);
-  // showGrid(segments);
+  showGrid(segments);
   
   fill(elc);
-  ellipse(x*(width/segments) + (width/segments/2),y*(width/segments)+(width/segments/2),width/segments,width/segments)
+  
+  ellipse((2*x+1)*gridsize/2,(2*y+1)*gridsize/2,gridsize,gridsize)
   
   // print(x,y);
   
@@ -67,10 +74,12 @@ function mousePressed() {
 function showGrid(n) {
   fill(0);
   strokeWeight(1);
-  for (let i = 0; i < width; i += width / n) {
+  
+  
+  for (let i = 0; i < width; i += gridsize) {
     line(i, 0, i, height);
   }
-  for (let i = 0; i < height; i += height / n) {
+  for (let i = 0; i < height; i += gridsize) {
     line(0, i, width, i);
   }
 }
